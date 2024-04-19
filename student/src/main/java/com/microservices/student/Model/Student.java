a6d9607c3fe0b5d8aaf463f14ff0c31e5d9b36f7
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-04-19 15:11:08 +0500

    updating configuration file and Creating student model

---
+++ student/src/main/java/com/microservices/student/Model/Student.java
@@ -0,0 +1,25 @@
+package com.microservices.student.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+
+    @Id
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Integer schoolId;
+}
