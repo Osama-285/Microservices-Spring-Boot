b30602e716247a07ad45692a50e8f486d406a7ba
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-04-19 15:58:18 +0500

    Adding Scool project

--- student/src/main/java/com/microservices/student/Controller/StudentController.java
+++ student/src/main/java/com/microservices/student/Controller/StudentController.java
@@ -1,6 +1,10 @@
 package com.microservices.student.Controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +20,16 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/v1/students")
 @RequiredArgsConstructor
 public class StudentController {
-    private StudentService service;
+    private final StudentService service;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void save(@RequestBody Student student) {
         service.saveStudent(student);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Student>> findAllStudents() {
+        return ResponseEntity.ok(service.findAllStudents());
     }
 }
