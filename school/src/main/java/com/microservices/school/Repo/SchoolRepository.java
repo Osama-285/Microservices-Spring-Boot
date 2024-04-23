509bd5c80d4e354199f749547f0fe5de9278851a
Muhammad Osama <61136831+Osama-285@users.noreply.github.com>
2024-04-23 07:31:16 +0500

    Removing school microservices

--- school/src/main/java/com/microservices/school/Repo/SchoolRepository.java
+++
@@ -1,9 +0,0 @@
-package com.microservices.school.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.microservices.school.Model.School;
-
-public interface SchoolRepository extends JpaRepository<School, Integer> {
-
-}
